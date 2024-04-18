6d16cfeb5c9c6d7d6c6dcc87bff04a8febe53591
enderNakamoto <ender.nakamoto@gmail.com>
2024-04-17 22:40:22 -0700

    initiating game states

---
+++ packages/chain/lib/models.ts
@@ -0,0 +1,35 @@
+import { Struct, Field, CircuitString } from 'o1js';
+
+
+export class PlanetaryDefense extends Struct({
+    battleships: Field,
+    destroyers: Field,
+    carriers: Field
+  }){
+    strength(){
+      const fleetStrength = this.battleships.add(this.destroyers).add(this.carriers);
+      return fleetStrength;
+    };
+  };
+
+  export class Fleet extends Struct({
+      battleships: Field,
+      destroyers: Field,
+      carriers: Field
+    }){
+      strength(){
+        const fleetStrength = this.battleships.add(this.destroyers).add(this.carriers);
+        return fleetStrength;
+      };
+    };  
+
+export class Planet extends Struct({
+    name: CircuitString,
+    facttion: Field,
+    defenseHash: Field,
+    incomingAttack: Fleet,
+    incomingAttackTime: Field,
+    points: Field,
+    locationHash: Field,
+    owner: Field,
+  }){};
---
+++ packages/chain/src/game.ts
@@ -0,0 +1,32 @@
+import { 
+    runtimeModule,
+    state, 
+    runtimeMethod, 
+    RuntimeModule 
+} from "@proto-kit/module";
+import { State, StateMap, assert } from "@proto-kit/protocol";
+import { Field, Bool, PublicKey } from "o1js";
+import { Planet } from "../lib/models.ts";
+
+@runtimeModule()
+export class GameRuntime extends RuntimeModule<unknown> {
+
+    // key is the locationHash, value shows if the planet is occupied
+    @state() public planetLedger  = StateMap.from<Field, Bool>(
+        Field, 
+        Bool
+    );
+
+    // key is the locationHash, value is the planet details
+    @state() public planetDetails = StateMap.from<Field, Planet>(
+        Field,
+        Planet
+    );
+
+    // key is the owner's public key, value is the planet locationHash
+    @state() public ownershipLedger = StateMap.from<PublicKey, Field>(
+        PublicKey,
+        Field
+    );
+
+}
